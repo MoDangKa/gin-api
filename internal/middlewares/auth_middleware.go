780d6a6e204d212b44b43e0f0bea3d5d501d7598
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-03-04 17:49:31 +0700

    perf: config auth

--- internal/middlewares/auth_middleware.go
+++ internal/middlewares/auth_middleware.go
@@ -1,14 +1,16 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 )
 
-// AuthMiddleware checks if the request has a valid authorization token
-func AuthMiddleware() gin.HandlerFunc {
+func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
@@ -17,28 +19,69 @@ func AuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		token := strings.TrimPrefix(authHeader, "Bearer ")
-		if token == "" {
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+		if tokenString == "" {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Token missing"})
 			c.Abort()
 			return
 		}
 
-		// TODO: Validate the token (e.g., using JWT)
-		isValid := validateToken(token)
-		if !isValid {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+		claims, err := validateToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 			c.Abort()
 			return
 		}
 
-		// If token is valid, proceed to the next handler
+		c.Set("claims", claims)
 		c.Next()
 	}
 }
 
-// validateToken validates the token (this is a placeholder function)
-func validateToken(token string) bool {
-	// Implement token validation logic here (e.g., using JWT)
-	return token == "valid-token" // Replace with actual validation logic
+func validateToken(tokenString string) (jwt.MapClaims, error) {
+	secretKey := []byte(os.Getenv("JWT_SECRET_KEY"))
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, fmt.Errorf("invalid token")
+	}
+}
+
+func IsAdmin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		claims, exists := c.Get("claims")
+		if !exists {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			c.Abort()
+			return
+		}
+
+		mapClaims, ok := claims.(jwt.MapClaims)
+		if !ok {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid claims"})
+			c.Abort()
+			return
+		}
+
+		isAdmin, ok := mapClaims["admin"].(bool)
+		if !ok || !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Access denied: admin privileges required"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
 }
