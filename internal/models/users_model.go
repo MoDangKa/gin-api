b369441760ef1c8ec42a4f3f605752bf466a9dd1
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-03-06 18:24:32 +0700

    perf(models): update users

--- internal/models/users_model.go
+++ internal/models/users_model.go
@@ -3,18 +3,28 @@ package models
 import "time"
 
 type User struct {
-	ID        int       `json:"id"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
-	Name      string    `json:"name"`
-	Photo     string    `json:"photo"`
-	Role      string    `json:"role"`
-	Active    bool      `json:"active"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID                   int       `json:"id"`
+	Email                string    `json:"email"`
+	Password             string    `json:"password"`
+	Name                 string    `json:"name"`
+	Photo                string    `json:"photo"`
+	Role                 string    `json:"role"`
+	Active               bool      `json:"active"`
+	PasswordResetToken   string    `json:"password_reset_toke"`
+	PasswordResetExpires time.Time `json:"password_reset_expires"`
+	CreatedAt            time.Time `json:"created_at"`
+	UpdatedAt            time.Time `json:"updated_at"`
 }
 
 type UserWithToken struct {
 	User  User   `json:"user"`
 	Token string `json:"token"`
+}
+
+type UserResponse struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
+	Name  string `json:"name"`
+	Photo string `json:"photo"`
+	Role  string `json:"role"`
 }
