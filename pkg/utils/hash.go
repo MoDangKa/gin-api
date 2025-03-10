d1c04f851d31d70183d3ff4f12b5f088536885ed
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-03-10 13:12:58 +0700

    feat(utils): create HashToken

--- pkg/utils/hash.go
+++ pkg/utils/hash.go
@@ -1,6 +1,10 @@
 package utils
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -15,4 +19,14 @@ func HashPassword(password string) (string, error) {
 func CheckPassword(password string, hashedPassword string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err == nil
+}
+
+func HashToken(token string) (string, error) {
+	hash := sha256.New()
+
+	if _, err := hash.Write([]byte(token)); err != nil {
+		return "", fmt.Errorf("error hashing token: %w", err)
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
