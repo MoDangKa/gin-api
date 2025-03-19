13249f2dded977554fe47c1d07dcae74a798c4be
Thanaphon Phumthan <68098235+MoDangKa@users.noreply.github.com>
2025-03-19 09:55:23 +0700

    perf: config LoadConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"log"
+	"os"
+)
+
 type Config struct {
 	ServerAddress string
 	DBHost        string
@@ -10,12 +15,42 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
+	serverAddress, ok := os.LookupEnv("SERVER_ADDRESS")
+	if !ok {
+		log.Fatal("SERVER_ADDRESS is required but not set")
+	}
+
+	DBHost, ok := os.LookupEnv("DB_HOST")
+	if !ok {
+		DBHost = "localhost"
+	}
+
+	DBPort, ok := os.LookupEnv("DB_PORT")
+	if !ok {
+		DBPort = "5432"
+	}
+
+	DBUser, ok := os.LookupEnv("DB_USER")
+	if !ok {
+		log.Fatal("DB_USER is required but not set")
+	}
+
+	DBPassword, ok := os.LookupEnv("DB_PASSWORD")
+	if !ok {
+		log.Fatal("DB_PASSWORD is required but not set")
+	}
+
+	DBName, ok := os.LookupEnv("DB_NAME")
+	if !ok {
+		log.Fatal("DB_NAME is required but not set")
+	}
+
 	return &Config{
-		ServerAddress: ":8080",
-		DBHost:        "localhost",
-		DBPort:        "5432",
-		DBUser:        "postgres",
-		DBPassword:    "1234",
-		DBName:        "gin_api",
+		ServerAddress: serverAddress,
+		DBHost:        DBHost,
+		DBPort:        DBPort,
+		DBUser:        DBUser,
+		DBPassword:    DBPassword,
+		DBName:        DBName,
 	}
 }
